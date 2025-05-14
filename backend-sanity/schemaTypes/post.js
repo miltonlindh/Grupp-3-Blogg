519a774fd030e0d199ea97b7629f7d7e5a21451b
miltonlindh <milton.lindh@live.com>
2025-05-14 13:58:38 +0200

    sanity.js och routing

---
+++ backend-sanity/schemaTypes/post.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } },
+    { name: 'author', title: 'Author', type: 'reference', to: { type: 'author' } },
+    { name: 'categories', title: 'Categories', type: 'array', of: [{ type: 'reference', to: { type: 'category' } }] },
+    { name: 'publishedAt', title: 'Published at', type: 'datetime' },
+    { name: 'body', title: 'Body', type: 'text' }
+  ]
+}
