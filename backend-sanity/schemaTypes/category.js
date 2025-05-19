63b449db96538c85bda411d9e1b70092b5e5f1f2
Daniel Arvebäck <daniel.arveback03@gmail.com>
2025-05-19 09:13:55 +0200

    backend sanity borttaget

--- backend-sanity/schemaTypes/category.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    { name: 'title', title: 'Title', type: 'string' },
-    { name: 'description', title: 'Description', type: 'text' }
-  ]
-}
