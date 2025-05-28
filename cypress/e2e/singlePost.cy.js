e4462ecd5fc31d99d4724cbc8442573ba0ae1630
miltonlindh <milton.lindh@live.com>
2025-05-28 11:17:49 +0200

    upd singlepost test

--- cypress/e2e/singlePost.cy.js
+++ cypress/e2e/singlePost.cy.js
@@ -4,17 +4,17 @@ describe('Enskilt inlägg / SinglePost', () => {
 
   it('visar ett inlägg när man besöker en slug', () => {
     //besöker ett specifikt inlägg
-    cy.visit('/post/Soffa');
+    cy.visit('/post/madeira-hidden-waterfall');
 
     //kontrollerar att titel visas
     cy.get('h1').should('exist');
 
     //kontrollerar att inläggstext eller body finns
-    cy.get('article').should('contain.text');
+    cy.get('article').invoke('text').should('not.be.empty');
   });
 
   it('visar tillbaka-länken längst ner', () => {
-    cy.visit('/post/test-inlagg');
+    cy.visit('/post/madeira-hidden-waterfall');
     cy.contains('← Tillbaka').should('have.attr', 'href', '/');
   });
 
