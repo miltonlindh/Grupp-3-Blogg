ec12445d068297e0f768bd7934dd23590d838353
miltonlindh <milton.lindh@live.com>
2025-05-28 09:29:04 +0200

    fix i kod

--- src/Pages/SinglePost.jsx
+++ src/Pages/SinglePost.jsx
@@ -73,11 +73,9 @@
 
 //   );
 // }
-
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getPostBySlug } from "../bloggData";
-
 import { PortableText } from '@portabletext/react';
 import "../components/Style/PostList.css";
 
@@ -96,12 +94,7 @@ export default function SinglePost() {
   if (!post) return <p>Hittade inte inlägg</p>;
 
   return (
-
-<>
-    <article>
-
     <article className="post-list">
-
       <h1>{post.title}</h1>
 
       {post.mainImage?.asset?.url && (
@@ -116,36 +109,6 @@ export default function SinglePost() {
         />
       )}
 
-
-<article className="post-list">
-  <h1>{post.title}</h1>
-  <div className="post-grid">
-
-  {post.mainImage?.asset?.url && (
-    <img
-      src={post.mainImage.asset.url}
-      alt={post.title}
-      style={{
-        maxWidth: "80%",   
-        display: "block",  
-        margin: "0 auto",
-      }}
-    />
-  )}
-
-  {Array.isArray(post.body) ? (
-    <PortableText value={post.body} />
-  ) : post.body ? (
-    <p style={{whiteSpace: "pre-line" }}>{post.body}</p>
-  ) : null }
-
-  <button className="back-btn">
-    <Link to="/" style={{color: "#0000FF"}}>← Tillbaka</Link>
-  </button>
-  </div>
-</article>
-</>
-
       {Array.isArray(post.body) ? (
         <PortableText value={post.body} />
       ) : post.body ? (
@@ -156,6 +119,5 @@ export default function SinglePost() {
         <Link to="/" style={{ color: "#0000FF" }}>← Tillbaka</Link>
       </button>
     </article>
-
   );
 }
