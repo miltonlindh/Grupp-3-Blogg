af46d03c6cd24fd4ee1acdb98874bd3836801054
miltonlindh <milton.lindh@live.com>
2025-05-30 09:26:39 +0200

    upd sanity

--- src/sanity.js
+++ src/sanity.js
@@ -2,9 +2,9 @@ import { createClient } from "@sanity/client";
 
 //skapar själva klienten som vi använder för att skicka data till sanity
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, //vårat id för projektet, hämtas från .env filen
-  dataset: import.meta.env.VITE_SANITY_DATASET, //vilket dataset vi jobbar mot
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION, //sanity versionen vi använder
-  useCdn: import.meta.env.VITE_SANITY_USE_CDN === 'true', //useCdn true som gör att vi använder sanitys cache
-  token: import.meta.env.VITE_SANITY_TOKEN //token används om vi ska kunna skapa uppdatera eller ta bort data
+  projectId: "h1nmn9gv", // Vårt projekt-ID från Sanity
+  dataset: "production", // Vilket dataset vi jobbar mot
+  apiVersion: "2023-01-01", // Sanity-version vi använder
+  useCdn: false, // Vi använder inte Sanitys cache för att alltid få färsk data
+  token: "skYtoKdclBmalqHsONDCdldqavttlXEek0Rx0Ur8KE4DKvb0bxp4ZE3PlL0I65N48Bz4VM4Z9mDsFlTgsTd24Tx1ic3YEm5TzozjNiUj5fsmahbAcA4tFhoUzRmdUaOEiNoWX2MrILYV7T5vIgcFPcOGkAspeloVjfUtta5Fx7cvvL0PDU8J" // Token för att kunna skapa/redigera inlägg från adminpanelen//token används om vi ska kunna skapa uppdatera eller ta bort data
 });
